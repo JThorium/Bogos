72765d4da33263ada2c30dcf7b8c167747030420
Jyah <jyahnorman@gmail.com>
2025-06-27 14:33:29 +0930

    Add AstroAnnihilatorAlpha8 HTML and V3 project files

---
+++ AstroAnnihilatorV3/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <OrbitControls />
+    </Canvas>
+  );
+}
+
+export default App;
